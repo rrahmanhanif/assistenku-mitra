988a756f81b73b4de0649e1d41e3dde778936f19
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-23 13:19:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
