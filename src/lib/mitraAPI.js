92ef5c6d3ae7b0d9b9d266d7a2f4cf088ec6431a
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 23:42:00 +0700

    Update mitraAPI.js

--- src/lib/mitraAPI.js
+++ src/lib/mitraAPI.js
@@ -1,24 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export const supabaseMitra = createClient(supabaseUrl, supabaseKey);
 
-// Buat Supabase client
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON
-);
-
-export async function updateMitraLocation(mitraId, lat, lng) {
-  return await supabase
+export async function updateMitraLocation(mitraId, latitude, longitude) {
+  return supabaseMitra
     .from("mitra_location")
     .upsert({
       mitra_id: mitraId,
-      latitude: lat,
-      longitude: lng,
-      updated_at: new Date().toISOString(),
+      latitude,
+      longitude,
+      updated_at: new Date().toISOString()
     });
 }
 
 export async function updateOrderStatus(orderId, status) {
-  return await supabase
+  return supabaseMitra
     .from("orders")
     .update({ status })
     .eq("id", orderId);
