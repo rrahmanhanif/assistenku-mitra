b6b2c0ea1cf5716184f2b0495961cba6e42c5b83
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-29 23:52:15 +0700

    Create chatMitra.js

---
+++ src/lib/chatMitra.js
@@ -0,0 +1,27 @@
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export async function sendMessage(orderId, sender, message) {
+  return supabase
+    .from("messages")
+    .insert([{ order_id: orderId, sender, message }]);
+}
+
+export function subscribeChat(orderId, callback) {
+  return supabase
+    .channel(`chat-order-${orderId}`)
+    .on(
+      "postgres_changes",
+      {
+        event: "INSERT",
+        schema: "public",
+        table: "messages",
+        filter: `order_id=eq.${orderId}`,
+      },
+      (payload) => callback(payload.new)
+    )
+    .subscribe();
+}
