7e782122bf62a7103d3d90c6d6642ef202b7d13a
rrahmanhanif <rrahmanhanif@gmail.com>
2026-01-01 19:19:31 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
-logEvent(analytics, "customer_app_opened");
-logEvent(analytics, "mitra_login_success");
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
