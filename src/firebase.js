309891a2c83494bc4dd3eaedf7780d0798babc19
rrahmanhanif <rrahmanhanif@gmail.com>
2026-02-04 02:17:23 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,5 @@
-// firebase.js (untuk Assistenku-Mitra)
+// Firebase integration removed in favor of centralized API.
+export const auth = null;
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAMrnOnJIWWs8zWUMDoYYs-95xo-LAVBbc",
-  authDomain: "assistenku-mitra.firebaseapp.com",
-  projectId: "assistenku-mitra",
-  storageBucket: "assistenku-mitra.firebasestorage.app",
-  messagingSenderId: "84136573544",
-  appId: "1:84136573544:web:c9b1a401e741799fbd59dd",
-  measurementId: "G-4LY2FQLGK1"
-};
-
-// Init Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export Auth supaya bisa dipakai login/daftar
-export const auth = getAuth(app);
-
+const app = null;
 export default app;
